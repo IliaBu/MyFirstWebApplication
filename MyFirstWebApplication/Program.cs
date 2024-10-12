de94aafb73e1a7d6bb1ba0ffa7aa0cd695fae949
IliaBu <bunamaksi@yandex.ru>
2024-10-12 11:15:21 +0500

    Добавьте файлы проекта.

---
+++ MyFirstWebApplication/Program.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyFirstWebApplication.Models;
+
+namespace MyFirstWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddSingleton<WeatherForecastHolder>();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
